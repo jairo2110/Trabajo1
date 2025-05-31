7d35b443c609fd75f486c22cca503a929ebc5bd2
Jairo <jairomg219810@gmail.com>
2025-05-30 21:35:17 -0500

    Primer commit: Subida inicial del proyecto

---
+++ Ejemplo 1/Ejemplo 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double velocidad, tiempo;
+            const double km1 = 111;
+            // datos
+            Console.WriteLine(" Ingrese la velocidad en km/h:  ");
+            velocidad = double.Parse(Console.ReadLine());
+            // proceso
+            tiempo = km1 / velocidad;
+            // salida
+            Console.WriteLine("El tiempo que demora de Lima a Cañete es de: " +  tiempo +  " horas");
+            Console.ReadKey();
+        }
+    }
+}
